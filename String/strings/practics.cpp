4c364b577cd8b5d5db65372219062b404d6dcc9d
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-07-16 08:34:20 +0530

    Refactor highest frequency character logic; add new practics.cpp for string concatenation example

---
+++ String/strings/practics.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    // string s="sanesh";
+    // cout<<s<<endl;
+    // string str;
+    // cout<<"enter string:";
+    // cin>>str;
+    // cout<<str;
+    // return 0;
+
+    string str="aaaeee";
+    string str2="sanesh";
+    cout<<str+str2;
+    // int i=0;
+    // int count=0;
+    // while(str[i]!='\0'){
+    //     if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'){
+    //         count++;
+    //     }
+    //     i++;
+    // }
+    // cout<<count;
+    // str[0]='b';
+    // cout<<str;
+    // cout<<str.length();
+
+}
