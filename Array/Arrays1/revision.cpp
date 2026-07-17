6f248bd97ae4efcb70f0dfb1f30aa662a48e839c
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-07-17 06:52:50 +0530

    Update .gitignore; refactor revision.cpp to include suffix array implementation

--- Array/Arrays1/revision.cpp
+++ Array/Arrays1/revision.cpp
@@ -1,26 +1,54 @@
 #include<iostream>
 #include<climits>
+#include<vector>
 using namespace std;
 
+void print(int* arr ,int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main() {
-    int arr[]={2,4,5,7,1,90,64,231,122};
+    int arr[]={2,4,5,7,1,90,64,65,78,89,98,231,122};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int max1=INT_MIN;
-    int max2=INT_MIN;
-
-    for(int i=0;i<n;i++){
-        if(arr[i]>max1){
-            max1=arr[i];
-        }
+    vector<int>v;
+    print(arr,n);
+    for(int i=n;i>=0;i--){
+        v.push_back(arr[i]);
     }
-    cout<<max1<<endl;
-
-    for(int i=0;i<n;i++){
-        if(arr[i]==max1) continue;
-        else if(arr[i]>max2){
-            max2=arr[i];
-        }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
     }
-    cout<<max2<<endl;
-    return 0;
+
+    // int * ptr=arr;
+    // for(int i=0;i<n;i++){
+    //     cout<<*ptr <<" ";
+    //     ptr++;
+    // }
+    // int max1=INT_MIN;
+    // int max2=INT_MIN;
+
+    // for(int i=0;i<n;i++){
+    //     if(arr[i]>max1){
+    //         max1=arr[i];
+    //     }
+    // }
+    // cout<<max1<<endl;
+
+    // for(int i=0;i<n;i++){
+    //     if(arr[i]==max1) continue;
+    //     else if(arr[i]>max2){
+    //         max2=arr[i];
+    //     }
+    // }
+    // cout<<max2<<endl;
+
+    // print(arr,n);
+    // int *p =&arr[7];
+    // cout<<p<<endl;
+    // return 0;
+
+    
 }
---
+++ prefix_sum/suffix.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=n-2;i>=0;i--){
+        arr[i]=arr[i]+arr[i+1];
+    }
+    cout<<"suffix array :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
