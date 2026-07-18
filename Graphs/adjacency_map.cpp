d9ae243b59653f5a119b592b3d24af27594407bc
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-07-18 07:22:08 +0530

    Refactor adjacency map implementation to use graph as a parameter in functions

--- Graphs/adjacency_map.cpp
+++ Graphs/adjacency_map.cpp
@@ -1,18 +1,57 @@
+// #include<iostream>
+// #include<vector>
+// #include<unordered_map>
+// using namespace std;
+
+//using bloble variable
+// vector<unordered_map<int,int> >graph;
+// int v;//no of vertices
+// void add_edge(int src,int dest,int wt, bool bi_dir=true){
+//     graph[src][dest]=wt;    
+//     if(bi_dir){
+//         graph[dest][src]=wt;
+//     }
+// }
+
+// void display(){
+//     for(int i=0;i<graph.size();i++){
+//         cout<<i<<" -> ";
+//         for(auto el : graph[i]){
+//             cout << "(" << el.first << ", " << el.second << ") ";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main() {
+//     cout<<"enter your size of vertex: ";
+//     cin>>v;
+//     graph.resize(v);//resize the graph into v
+//     int e;
+//     cout<<"enter your size of edges :";
+//     cin>>e;
+//     while (e--){
+//         int s,d,wt;
+//         cout<<"enter source and destination and weight : ";
+//         cin>>s>>d>>wt;
+//         add_edge(s,d,wt);
+//     }
+//     display();
+//     return 0;
+// }
+
+
 #include<iostream>
 #include<vector>
 #include<unordered_map>
 using namespace std;
-
-vector<unordered_map<int,int> >graph;
-int v;//no of vertices
-void add_edge(int src,int dest,int wt,bool bi_dir=true){
+void add_edge(vector<unordered_map<int,int> >&graph,int src,int dest,int wt, bool bi_dir=true){
     graph[src][dest]=wt;    
     if(bi_dir){
         graph[dest][src]=wt;
     }
 }
 
-void display(){
+void display(vector<unordered_map<int,int> >&graph){
     for(int i=0;i<graph.size();i++){
         cout<<i<<" -> ";
         for(auto el : graph[i]){
@@ -22,8 +61,10 @@ void display(){
     }
 }
 int main() {
+    int v;//no of vertices
     cout<<"enter your size of vertex: ";
     cin>>v;
+    vector<unordered_map<int,int> >graph(v);
     graph.resize(v);//resize the graph into v
     int e;
     cout<<"enter your size of edges :";
@@ -32,8 +73,8 @@ int main() {
         int s,d,wt;
         cout<<"enter source and destination and weight : ";
         cin>>s>>d>>wt;
-        add_edge(s,d,wt);
+        add_edge(graph,s,d,wt);
     }
-    display();
+    display(graph);
     return 0;
 }
