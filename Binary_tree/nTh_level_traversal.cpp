e9571e56fa32891ad89f0f90d90c00c3dbb0ce64
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-07-22 14:43:44 +0530

    Refactor nth level traversal functions and enhance level order traversal implementation

--- Binary_tree/nTh_level_traversal.cpp
+++ Binary_tree/nTh_level_traversal.cpp
@@ -11,15 +11,43 @@ public:
         this->right=nullptr;
     }
 };
+
+int level(Node* root){
+    if(root==nullptr) return 0;
+    return 1+max(level(root->left),level(root->right));
+}
 void nthLevel(Node* root,int curr,int level){
     if(root==nullptr) return;
     if(curr==level){
         cout<<root->val<<" ";
+        return;//ye satate likh dene ke badh extra call nhi hoga
     }
     nthLevel(root->left,curr+1,level);
     nthLevel(root->right,curr+1,level);
 }
+//right to left
+void nthLevel2(Node* root,int curr,int level){
+    if(root==nullptr) return;
+    if(curr==level){
+        cout<<root->val<<" ";
+        return;//ye satate likh dene ke badh extra call nhi hoga
+    }
+    nthLevel2(root->right,curr+1,level);
+    nthLevel2(root->left,curr+1,level);
+}
+void levelOrder(Node* root){
 
+    int n=level(root);
+
+    for(int i=0;i<n;i++){
+        nthLevel(root,0,i);
+        cout<<endl;
+    }
+     for(int i=0;i<n;i++){
+        nthLevel2(root,0,i);
+        cout<<endl;
+    }
+}
 
 int main() {
     Node* a=new Node(1);
@@ -36,6 +64,10 @@ int main() {
     b->right=e;
     c->left=f;
     c->right=g;
-    nthLevel(a,0,2);
+   // nthLevel(a,0,2);
+
+   levelOrder(a);
+   cout<<endl;
+   levelOrder(a);
     return 0;
 }
