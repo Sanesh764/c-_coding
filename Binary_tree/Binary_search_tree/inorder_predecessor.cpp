3db43f29c55e918967f71160f4cc3cc8cacbc3db
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-07-31 06:19:53 +0530

    Add inorder predecessor function and example usage in binary search tree

---
+++ Binary_tree/Binary_search_tree/inorder_predecessor.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val=val;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+};
+
+int inorder_predecessor(Node* root){
+    if(root->left==nullptr) return 0;
+    Node* pre=root->left;
+    while (pre->right!=nullptr){
+        pre=pre->right;
+    }
+    return pre->val;
+    
+}
+
+int main() {
+    Node* a=new Node(10);
+    Node* b=new Node(5);
+    Node* c=new Node(20);
+    Node* d=new Node(2);
+    Node* e=new Node(8);
+    Node* f=new Node(15);
+    Node* g=new Node(25);
+    Node* h=new Node(4);
+    Node* i=new Node(12);
+    Node* j=new Node(3);
+    Node* k=new Node(13);
+
+    a->left=b;
+    a->right=c;
+    b->left=d;
+    b->right=e;
+    c->left=f;
+    c->right=g;
+    d->right=h;
+    f->left=i;
+    h->left=j;
+    i->right=k;
+
+    cout<<inorder_predecessor(a);
+    return 0;
+}
