3c0a4f162d868ffb8687b77860ccb3fd1cf4bcc1
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-08-07 06:19:13 +0530

    Refactor code structure for improved readability and maintainability

--- Bitwise_operators/binary_to_desimel.cpp
+++ Bitwise_operators/binary_to_desimel.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 using namespace std;
-int binary_to_desimal(string &binary){
-    int n=binary.size();
+int Binary_to_desimal(string &str){
+    int n=str.size();
     int result=0;
     for(int i=n-1;i>=0;i--){
-        char ch=binary[i];
-        int num=ch-'0';
-        result = result +num*(1<<(n-i-1));
+        char ch=str[i];
+        int nums=ch-'0';
+        result=result+nums*(1<<(n-i-1));
     }
     return result;
 }
 int main(){
-    string str="011101";
-    cout<<binary_to_desimal(str)<<endl;
+    string str="0111";
+    cout<<"the given binary number "<<str<<" of desimal value is : -> ";
+    cout<<Binary_to_desimal(str)<<endl;
 }
