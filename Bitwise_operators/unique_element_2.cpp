4b737bd69de5614ae901dd4dd81675d8c70baf13
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-08-08 05:58:27 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Bitwise_operators/unique_element_2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void findUnique(int * arr,int n){
+    int res=0;
+    for(int i=0;i<n;i++){
+        res=res^arr[i];
+    }
+    int temp=res;
+    int k=0;
+    while(true){
+        if((temp&1)==1){
+            break;
+        }
+        temp=temp>>1;
+        k++;
+    }
+    int retval=0;
+
+    for(int i=0;i<n;i++){
+        int num=arr[i];
+        if(((num>>k)&1)==1) retval ^=num;
+    }
+    cout<<retval<<" ";
+    res=retval ^res;
+    cout<<res<<" ";
+};
+int main() {
+    int arr[]={1,2,1,3,2,5,3,5,9,11};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    findUnique(arr,n);
+    return 0;
+}
