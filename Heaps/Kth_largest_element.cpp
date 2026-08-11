18a4222b094e69a98f45707221abea1a2e5e49be
Sanesh764 <Sanesh764@users.noreply.github.com>
2026-08-11 06:19:35 +0530

    Update kthsmallest.cpp to find the 3rd smallest element and add Kth_largest_element.cpp for finding the Kth largest element

--- Heaps/Kth_largest_element.cpp
+++ Heaps/Kth_largest_element.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int main() {
-    int arr[]={10,20,-4,5,18,24,1,-7,56};
-    int k=3;
+    int arr[]={11,4,24,-12,87,-6,23,4,1,23,-90,20};
     int n=sizeof(arr)/sizeof(arr[0]);
-    priority_queue<int> pq;
+    int k=n-1;
+    priority_queue<int,vector<int>,greater<int> >pq;
+
     for(int i=0;i<n;i++){
         pq.push(arr[i]);
         if(pq.size()>k) pq.pop();
     }
-    cout<<pq.top();
+    cout<<pq.top()<<endl;
     return 0;
 }
